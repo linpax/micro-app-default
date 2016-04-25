eb2989f5ff7b65b99c7c5b4384a5b2df18a403a7
Oleg Lunegov <zcasperx@gmail.com>
2016-04-25 08:53:52 +0600

    update config
    upgrade to new DI

--- app/configs/components/db.php
+++ app/configs/components/db.php
@@ -4,10 +4,12 @@
 return [
     'class' => '\Micro\Db\DbConnection',
     'arguments' => [
-        'connectionString' => 'mysql:host=localhost;dbname=micro',
-        'username' => 'micro',
-        'password' => 'micro',
+        'container' => '@this',
+        'config' => [
+            'connectionString' => 'mysql:host=localhost;dbname=micro',
+            'username' => 'micro',
+            'password' => 'micro',
+        ],
         'options' => [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''],
-        'container' => '@this'
     ]
 ];
